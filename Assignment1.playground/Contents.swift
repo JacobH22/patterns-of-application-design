80eb55450e3cfa4ba87ae487397e33a59b31eb50
Jacob Hunsaker <jacob.hunsaker@smail.astate.edu>
2019-10-21 01:18:50 -0500

    Adding my playground file to GitHub.

---
+++ Assignment1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let firstName = "Jacob"
+let lastName = "Hunsaker"
+let age = 22
+let birthPlace = "Pine Bluff, Arkansas"
+let careerAspirations = "I want to work in the video game industry for a studio like Santa Monica Studios or Insomniac Games."
+
+let bio = "\(firstName) \(lastName) \n\(age) \n\(birthPlace) \n\(careerAspirations)"
+print(bio)
