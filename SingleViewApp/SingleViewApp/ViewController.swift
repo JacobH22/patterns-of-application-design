8c4fa6cd85b302d4ffd49e1e437dbcdeb0d57b8a
Jacob Hunsaker <jacob.hunsaker@smail.astate.edu>
2019-10-27 15:59:29 -0500

    submitting photo app

---
+++ SingleViewApp/SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Jacob Hunsaker on 10/27/19.
+//  Copyright © 2019 Jacob Hunsaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
