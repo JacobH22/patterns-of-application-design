cbce7cd77f38e278fffe81fab0e2e9105c6e90d5
JacobH22 <54431858+JacobH22@users.noreply.github.com>
2019-11-03 20:15:27 -0600

    Add files via upload

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Jacob Hunsaker on 11/3/19.
+//  Copyright © 2019 Jacob Hunsaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var fieldOne: UITextField!
+    @IBOutlet weak var fieldTwo: UITextField!
+    
+    @IBAction func operatorPressed(_ sender: Any) {
+    }
+    
+}
+
